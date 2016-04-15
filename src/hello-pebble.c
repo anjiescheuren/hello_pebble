4003347534804407998e0b3f1c84bbea837bffb3
Anjie Scheuren <emailanjie@gmail.com>
2016-04-15 15:29:43 -0500

    hello, pebble

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -1,10 +1,24 @@
 #include <pebble.h>
 
 Window *window;
+TextLayer *text_layer;
 
 void init() {
   // Create the Window
   window = window_create();
+
+  //Create the TextLayer, for display at (0,0)
+  //and with a size of 144 x 40
+  text_layer = text_layer_create(GRect( 0, 0, 144, 40));
+
+  //Set the text that the TextLayer will display
+  text_layer_set_text(text_layer, "Hello, Pebble");
+
+  //Add as child layer to be included in rendering
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+  //Destroy text layer
+  text_layer_destroy(text_layer);
 
   // Push to the stack, animated
   window_stack_push(window, true);
